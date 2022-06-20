2c26ea7ecc82ab29f0c5dcf5cfcfd8e33b803f9a
boykopb@gmail.com <boykopb@gmail.com>
2022-06-20 20:17:33 +0300

    Added movement logic by mouse

---
+++ PiratesProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+  public void RestartLevel()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+  
+  
+  
+}
