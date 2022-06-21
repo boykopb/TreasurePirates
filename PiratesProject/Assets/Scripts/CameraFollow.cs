94b7763a93226cea3398f3511bb00304d0f06d56
boykopb@gmail.com <boykopb@gmail.com>
2022-06-21 20:57:27 +0300

    Added camera shake feedback

--- PiratesProject/Assets/Scripts/CameraFollow.cs
+++ PiratesProject/Assets/Scripts/CameraFollow.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
-    [SerializeField] private float _lerpRate = 5f;
+  [SerializeField] private Transform _target;
+  [SerializeField] private float _maxPositionX = 1.5f;
 
-    private void Update()
-    {
-        if (_target == null)
-            return;
-      
-        transform.position = Vector3.Lerp(transform.position, _target.position, Time.deltaTime * _lerpRate);
-    }
+  [SerializeField] private float _lerpRate = 5f;
+
+  private void Update()
+  {
+    if (_target == null)
+      return;
+
+    //cameraMovement without restriction
+    //  transform.position = Vector3.Lerp(transform.position, _target.position, Time.deltaTime * _lerpRate);
+
+    //cameraMovement with restriction
+    var nextPosition = Vector3.Lerp(transform.position, _target.position, Time.deltaTime * _lerpRate);
+    nextPosition.x = Mathf.Clamp(nextPosition.x, -_maxPositionX, _maxPositionX);
+    transform.position = nextPosition;
+  }
 }
