28551a795e40faddaaab04417731d6143f536585
boykopb@gmail.com <boykopb@gmail.com>
2022-06-22 11:17:37 +0300

    Minor fix (pink materials)

--- PiratesProject/Assets/Imported/Bitgem/StylisedWater/URP/Scripts/Bitgem/VFX/StylisedWater/WateverVolumeFloater.cs
+++ PiratesProject/Assets/Imported/Bitgem/StylisedWater/URP/Scripts/Bitgem/VFX/StylisedWater/WateverVolumeFloater.cs
@@ -8,27 +8,27 @@ using UnityEngine;
 
 namespace Bitgem.VFX.StylisedWater
 {
-    public class WateverVolumeFloater : MonoBehaviour
+  public class WateverVolumeFloater : MonoBehaviour
+  {
+    #region Public fields
+
+    public WaterVolumeHelper WaterVolumeHelper = null;
+
+    #endregion
+
+    #region MonoBehaviour events
+
+    void Update()
     {
-        #region Public fields
+      var instance = WaterVolumeHelper ? WaterVolumeHelper : WaterVolumeHelper.Instance;
+      if (!instance)
+      {
+        return;
+      }
 
-        public WaterVolumeHelper WaterVolumeHelper = null;
-
-        #endregion
-
-        #region MonoBehaviour events
-
-        void Update()
-        {
-            var instance = WaterVolumeHelper ? WaterVolumeHelper : WaterVolumeHelper.Instance;
-            if (!instance)
-            {
-                return;
-            }
-
-            transform.position = new Vector3(transform.position.x, instance.GetHeight(transform.position) ?? transform.position.y, transform.position.z);
-        }
-
-        #endregion
+      transform.position = new Vector3(transform.position.x, instance.GetHeight(transform.position) ?? transform.position.y, transform.position.z);
     }
+
+    #endregion
+  }
 }
--- PiratesProject/Assets/Scripts/CameraFollow.cs
+++ PiratesProject/Assets/Scripts/CameraFollow.cs
@@ -3,21 +3,24 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
   [SerializeField] private Transform _target;
-  [SerializeField] private float _maxPositionX = 1.5f;
-
-  [SerializeField] private float _lerpRate = 5f;
+  [SerializeField] private bool _isCameraMovementXRestricted = true;
+  [SerializeField] private float _maxPositionX = 1f;
+  [SerializeField] private float _lerpRate = 3f;
 
   private void Update()
   {
     if (_target == null)
       return;
 
-    //cameraMovement without restriction
-    //  transform.position = Vector3.Lerp(transform.position, _target.position, Time.deltaTime * _lerpRate);
-
-    //cameraMovement with restriction
-    var nextPosition = Vector3.Lerp(transform.position, _target.position, Time.deltaTime * _lerpRate);
-    nextPosition.x = Mathf.Clamp(nextPosition.x, -_maxPositionX, _maxPositionX);
-    transform.position = nextPosition;
+    if (!_isCameraMovementXRestricted)
+    {
+      transform.position = Vector3.Lerp(transform.position, _target.position, Time.deltaTime * _lerpRate);
+    }
+    else
+    {
+      var nextPosition = Vector3.Lerp(transform.position, _target.position, Time.deltaTime * _lerpRate);
+      nextPosition.x = Mathf.Clamp(nextPosition.x, -_maxPositionX, _maxPositionX);
+      transform.position = nextPosition;
+    }
   }
 }
